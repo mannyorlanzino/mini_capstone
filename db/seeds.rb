85bdfd067524cbcbb14194db3ad85022ffedbe6c
Manny <manuel.orlanzino@gmail.com>
2019-06-25 19:02:47 -0500

    create, migrate, and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
+Product.create(name: "Everlasting Gobstopper", price: 100, image_url: "https://images2.sw-cdn.net/product/picture/710x528_13586243_545922_1513562644.jpg", description: "THE Gobstopper. It laster. Like, forever.")
 
+Product.create(name: "Wonka Bar", price: 3, image_url: "https://images-na.ssl-images-amazon.com/images/I/71yT10p-HyL._SX425_.jpg", description: "Wonka chocolate. Nuff said. Maybe there's a golden ticket inside?")
+
+Product.create(name: "Golden Egg", price: 1000, image_url: "https://i.pinimg.com/originals/46/35/0e/46350e11dd3e91ce2956d8f94d2cc466.jpg", description: "An egg made of solid gold.")
