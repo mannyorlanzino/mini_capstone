3ae324f394f3f55cec18d80758dfe6b905885f03
Manny <manuel.orlanzino@gmail.com>
2019-06-26 19:11:09 -0500

    Add query and segment param routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,16 @@ class Api::ProductsController < ApplicationController
     @allproducts = Product.all
     render "allproducts.json.jb"
   end
+
+  # def product # Using array index
+  #   id = params["id"].to_i - 1
+  #   @product = Product.all[id]
+  #   render "product.json.jb"
+  # end
+
+  def product # Using .find_by method
+    product_id = params["id"]
+    @product = Product.find_by(id: product_id)
+    render "product.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
+# @product_urls = []
+# Product.all do |product|
+#   @product_urls << product["name"].downcase.delete(" ")
+# end
+
 Rails.application.routes.draw do
   namespace :api do
     get "/allproducts" => "products#allproducts"
+    get "/product" => "products#product"
+    get "/product/:id" => "products#product"
   end
 end
