7d1b9ec1abfa5fd18e73b40962dad5ae6c454a1e
Manny <manuel.orlanzino@gmail.com>
2019-06-27 19:40:11 -0500

    Add remaining RESTful routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::ProductsController < ApplicationController
   def index
-    @allproducts = Product.all
+    @allproducts = Product.all.sort
     render "index.json.jb"
   end
 
@@ -18,5 +18,22 @@ class Api::ProductsController < ApplicationController
   def show # Using .find_by method
     @product = Product.find_by(id: params["id"])
     render "show.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params["id"])
+    @product.id = params["id"] || @product.id
+    @product.name = params["name"] || @product.name
+    @product.price = params["price"] || @product.price
+    @product.image_url = params["image_url"] || @product.image_url
+    @product.description = params["description"] || @product.description
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params["id"])
+    @product.destroy
+    render json: "Product has been destroyed."
   end
 end
