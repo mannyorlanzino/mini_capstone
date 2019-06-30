91b14c68014ef9a2398885c4d2dfcdbd35190ed1
Manny <manuel.orlanzino@gmail.com>
2019-06-30 10:28:02 -0500

    Add tax, total, and is_discounted methods to Product model  and views

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    price <= 10
+  end
+
+  def tax
+    tax_rate = 0.09
+    tax = tax_rate * price
+  end
+
+  def total
+    price + tax
+  end
 end
